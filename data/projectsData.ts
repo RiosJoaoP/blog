bbe712b34dfc7b7e42458a7b4534c3e71cce1814
João Paulo Rios <joao.paulo.rios@hotmail.com>
2024-03-26 21:08:26 -0300

    content: changing image folders structure

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -9,7 +9,7 @@ const projectsData: Project[] = [
   {
     title: 'DEMIBAV - Gitec',
     description: `Aim to reduce injection mold costs by employing steel insert gates - a permanent component - with inserts manufactured through additive manufacturing using low-cost materials, and inserts through machining of low-cost metallic alloy (e.g., Aluminum, ZAMAC, etc.) - a replaceable component.`,
-    imgSrc: '/static/images/gitec.jpeg',
+    imgSrc: '/static/images/projects/gitec.jpeg',
   },
 ]
 
